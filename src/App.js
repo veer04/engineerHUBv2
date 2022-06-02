d595d48afe3a3bb4dc2cf801e0400facdae1f69a
Aditi Jaiswal <jaisaditi19@gmail.com>
2022-06-02 23:54:38 +0530

    added-hiring-page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
  import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import HomePage from './Components/HomePage/HomePage';
+import Hiring from './Components/Hiring/Hiring';
 
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
-     <HomePage/>
+     {/* <HomePage/> */}
+     <Hiring/>
     </div>
   );
 }
