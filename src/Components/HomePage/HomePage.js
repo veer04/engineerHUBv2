8eca4acdb98f5df2406e5954212e4f195a9807d6
Manish-rai-dev <raimanishkumar52@gmail.com>
2022-06-07 16:29:59 +0530

    commit the main page

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
-import'../HomePage/HomePage.css'
+import React from 'react';
+import ReactPlayer from 'react-player';
+import'../HomePage/HomePage.css';
+import '../Aboutus/Aboutus';
+import Aboutus from '../Aboutus/Aboutus';
+import '../Whatwedo/Whatwedo';
+import Whatwedo from '../Whatwedo/Whatwedo';
+import '../Magzine/Magzine';
+import Magzine from '../Magzine/Magzine';
+import '../Events/Events'
+import Events from '../Events/Events';
+import '../Courses/Courses'
+import Courses from '../Courses/Courses';
+import '../Members/Members';
+import Members from '../Members/Members';
+import '../Footer/Footer';
+import Footer from '../Footer/Footer';
+
 
 
 export default function NavBar() {
   return (
+    <>
     <div>
     <div className='home'>
       <div className='left'>
@@ -19,7 +35,33 @@ export default function NavBar() {
       <div className='line'>
       <h2>" A VERSATILE COMMUNITY FOR VERSATILE PEOPLE "</h2>
       <button className='arrow'>v</button>
+
       </div>
     </div>
+    <div className="about">
+      <Aboutus/>
+
+    </div>
+    <div className="WhatWeDo">
+      <Whatwedo/>
+
+    </div>
+    
+  <div className="magzine">
+    <Magzine/>
+  </div>
+  <div className="Events">
+    <Events/>
+  </div>
+  <div className="Courses">
+    <Courses/>
+  </div>
+  <div className="Members">
+    <Members/>
+  </div>
+  <div className="Footer">
+    <Footer/>
+  </div>
+    </>
   )
 }
---
+++ src/Components/Magzine/Magzine.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import '../Magzine/Magzine.css'
+import ShareIcon from '@mui/icons-material/Share';
+import backImage from '../Magzine/backimg.png'
+import { borderRadius } from '@mui/system';
+
+function Magzine() {
+  return (
+<>
+<div className="content">
+    <div className="container">
+        <h1 className='text1'>
+        Magzines & Hand-Book
+        </h1>
+        <h5 className='text2 text111 '>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Quos, natus. Nihil molestias culpa quibusdam quaerat ea
+              neque velit fugit officia amet eligendi! Quis quos animi
+               officia explicabo accusamus obcaecati totam.
+        </h5>
+
+     <div className="row containmentz">
+     
+         <div className="col-lg-2 ">
+         <div className="card" style={{ width: '18rem', borderRadius: 25}}>
+         <img src={backImage} className="card-img-top" alt="..."/>
+  <div className="card-img-top"></div>
+  <div className="card-body">
+    <h5 className="card-title">App
+Development <ShareIcon style={{marginLeft: 5}}/></h5>
+    
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Pharetra consequat consequat at fermentum sollicitudin pellentesque tortor..</p>
+  
+  </div>
+</div>
+<button className='buttonz'>Open</button>
+         </div>
+         <div className="col-lg-1"></div>
+         <div className="col-lg-2">
+         <div className="card" style={{ width: '18rem', borderRadius: 25}}>
+  <img src={backImage} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title"> UI / UX - 
+Designing <ShareIcon style={{marginLeft: 5}}/></h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Pharetra consequat consequat at fermentum sollicitudin pellentesque tortor..</p>
+  
+  </div>
+</div>
+<button className='buttonz'>Open</button>
+             </div>
+             <div className="col-lg-1"></div>
+             <div className="col-lg-2">
+             <div className="card"style={{ width: '18rem', borderRadius: 25}} >
+             <img src={backImage} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Web
+Development <ShareIcon style={{marginLeft: 10}}/></h5>
+    <p className="card-text ">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Pharetra consequat consequat at fermentum sollicitudin pellentesque tortor..</p>
+
+  </div>
+</div>
+<button className='buttonz'>Open</button>
+             </div>
+             <div className="col-lg-1"></div>
+             <div className="col-lg-2">
+             <div className="card " style={{ width: '18rem', borderRadius: 25}}>
+             <img src={backImage} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Cyber -
+Security <ShareIcon style={{marginLeft: 30}}/></h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Pharetra consequat consequat at fermentum sollicitudin pellentesque tortor..</p>
+  
+  </div>
+</div><button className='buttonz'>Open</button>
+             </div>
+             
+         </div>   
+    </div>
+</div>
+</>
+  )
+}
+
+export default Magzine
