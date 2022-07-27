4276cb9016a84b58de2cdd1d0c92b6b2a4f26a85
Rahul <rajamohanty12345@gmail.com>
2022-07-27 22:31:50 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import HomePage from './Components/HomePage/HomePage';
 // import MagzineAndHandbook from './Components/magzineandHandbook/magzineAndHandbook';
-
+import Internship from './Components/Internship/Internship';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
-     <HomePage/>
+     <Internship/>
      
     </div>
   );
