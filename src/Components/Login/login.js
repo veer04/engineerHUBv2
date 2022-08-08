935fc84d4c5aff5dfa2b35b6204a9d4e8609fd34
Rahul <rajamohanty12345@gmail.com>
2022-08-08 17:34:46 +0530

    login ui updated

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,48 +1,48 @@
 import React from "react";
 import "./login.css";
 import google from "./svg/google.svg";
-import github from "./svg/github.svg";
-import linkedin from "./svg/linkedin.svg";
+import fb from "./svg/fb.svg";
 import LefImg from "./left-img.jpg";
+import Ehub from "./svg/Ehub.svg";
 const login = () => {
   return (
     <div className="cont">
-    
-        <div className="mytagContainer">engineerHUB</div>
-        <h5 className="myheaderbelow">Students . Campus . Industries</h5>
+      <div className="cont-head">
+        <img src={Ehub} alt="Ehub" />
+        <div className="my-form-head">Let's you in</div>
+      </div>
 
-        <div className="my-form">
-          <div className="form-cont">
-            <label>Username</label>
-            <input type="text" />
-          </div>
-          <div className="form-cont">
-            <label>Password</label>
-            <input type="password" />
-          </div>
-          <div className="f-p">Forgot Password ?</div>
-          <div className="form-opt">
-            <div className="my-btn si">Log in</div>
+      <div className="sign-field">
+        <div className="sign-opt ">
+          <img src={google} alt="google" />
+          Continue with Google
+        </div>
+        <div className="sign-opt ">
+          <img src={fb} alt="Facebook" />
+          Continue with Facebook
+        </div>
+      </div>
+      <div className="divisor">
+        <hr/>OR<hr/>
+      </div>
+      <div className="my-form">
+        {/* <div className="form-cont">
+          <label>Username</label>
+          <input type="text" />
+        </div>
+        <div className="form-cont">
+          <label>Password</label>
+          <input type="password" />
+        </div> */}
+        {/* <div className="f-p">Forgot Password ?</div> */}
+        <div className="form-opt">
+          <div className="my-btn si">Sign in with Password</div>
+          <div className="my-item-cont">
+            <div>Didn't have an account?</div>
+            <div className="f-p ">Sign up</div>
           </div>
         </div>
-        <div className="divisor">OR</div>
-        <div className="sign-field">
-          <div className="sign-opt gh">
-            <img src={github} alt="github" />
-            Github
-          </div>
-          <div className="sign-opt gg">
-            <img src={google} alt="google" />
-            Google
-          </div>
-          <div className="sign-opt lin">
-            <img src={linkedin} alt="linkedin" />
-            Linkedin
-          </div>
-        </div>
-
-        <div className="my-border"></div>
- 
+      </div>
     </div>
   );
 };
