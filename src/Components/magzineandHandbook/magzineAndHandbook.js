05cba5c273d9263545c4fa10a6c0e1d94e50480a
Aditi Jaiswal <jaisaditi19@gmail.com>
2022-08-12 12:05:56 +0530

    update

--- src/Components/magzineandHandbook/magzineAndHandbook.js
+++ src/Components/magzineandHandbook/magzineAndHandbook.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "../magzineandHandbook/magzineandhandbook.css";
 import MagzineAndHandbookcard from "./magzinehandbookcaed";
+import Full from "../freecourses-pages/Full";
 export default function MagzineAndHandbook() {
   return (
     <>
       <div className="container-fluid imp">
-        <div className="magzine-container">
-          <div className="magzine-header">
-            <h2>Magzines &amp; HandBook</h2>
-            <p>
+        <div className="container-courses">
+          <div className="heading">
+            Magzines &amp; HandBook 
+            </div>
+            <p className="texthire">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim
               ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
               aliquip ex ea commodo consequat.
             </p>
-          </div>
+        
 
           <div className="magzine-card-container">
+            {/* <MagzineAndHandbookcard />
             <MagzineAndHandbookcard />
-            <MagzineAndHandbookcard />
-            <MagzineAndHandbookcard />
+            <MagzineAndHandbookcard /> */}
+            <Full/>
           </div>
         </div>
       </div>
