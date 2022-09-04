731f4262e26e0c25dba6b0ad829aa4e4bb560e93
Rahul <rajamohanty12345@gmail.com>
2022-09-04 22:00:16 +0530

    all apis integrated

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import MagzineAndHandbook from "./Components/magzineandHandbook/magzineAndHandbo
 import Internship from "./Components/Internship/Internship";
 import Hiring from "./Components/Hiring/Hiring";
 import Campus from "./Components/Campus/Campus";
-
+import UserPage from "./Components/UserPage/user"
 import Mentors from "./Components/Mentors/Mentor";
 import Teams from "./Components/Teams/Team";
 import IndustryPersona from "./Components/IndustryPersona/IndustryPersona";
 import Freecourses from "./Components/freecourses-pages/Freecourses";
 import Resources from "./Components/Resources/Resources";
 import Test from "./APIs/TestAPI";
-import Login from "./Components/Login/login"
-import Register from "./Components/Login/Register"
-import SignUp from "./Components/Login/Signup"
+import Login from "./Components/Login/login";
+import Register from "./Components/Login/Register";
+import SignUp from "./Components/Login/Signup";
 import Footer from "./Components/Footer/Footer";
 function App() {
   return (
@@ -23,8 +23,6 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="" exact element={<HomePage />} />
-
-
 
         <Route path="/internship" exact element={<Internship />} />
         <Route path="/magazine" exact element={<MagzineAndHandbook />} />
@@ -34,13 +32,14 @@ function App() {
         <Route path="/mentor" exact element={<Mentors />} />
         <Route path="/Team" exact element={<Teams />} />
         <Route path="/industry" exact element={<IndustryPersona />} />
+        <Route path="/userpage" exact element={<UserPage />} />
 
         <Route path="/courses" exact element={<Freecourses />} />
         <Route path="/resources" exact element={<Resources />} />
-        <Route path="/test" exact element={<Test/>}/>
-        <Route path="/login" exact element={<Login/>}/>
-        <Route path="/register" exact element={<Register/>}/>
-        <Route path="/signup" exact element={<SignUp/>}/>
+        <Route path="/test" exact element={<Test />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/register" exact element={<Register />} />
+        <Route path="/signup" exact element={<SignUp />} />
       </Routes>
       <div className="Footer">
         <Footer />
