e8b5fc01fe50befc729c00c9832b3e1dd4bfd70e
Rahul <rajamohanty12345@gmail.com>
2022-09-12 00:24:36 +0530

    ui updated responsive view

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Swiper from "react-slider-swiper";
+
 import ReactPlayer from "react-player";
 
 import "../HomePage/HomePage.css";
@@ -32,19 +32,7 @@ import CountUp from "react-countup";
 // const About = React.lazy(()=>import('../Aboutus/Aboutus'));
 // const About = lazy(() => import('../Aboutus/Aboutus'));
 
-
-
-
 export default function NavBar() {
-  const params = {
-    pagination: ".swiper-pagination",
-    paginationClickable: true,
-    slidesPerView: 4,
-    runCallbacksOnInit: true,
-    onInit: (swiper) => {
-      this.swiper = swiper;
-    },
-  };
   return (
     <>
       <div className="row headearpart">
@@ -57,21 +45,55 @@ export default function NavBar() {
             <div className="col-4 headercard">
               <img src={campusIcon} alt="Campus" />
               {/* <h1 className="headercardtext">750+</h1> */}
-              <h1 className="headercardtext"><CountUp start={500} end={750} className="headercardtext" duration={0.7} afterEffects={true} smooth={true} smartEasingAmount={true} delay={0}></CountUp>+</h1> 
-              
+              <h1 className="headercardtext">
+                <CountUp
+                  start={500}
+                  end={750}
+                  className="headercardtext"
+                  duration={0.7}
+                  afterEffects={true}
+                  smooth={true}
+                  smartEasingAmount={true}
+                  delay={0}
+                ></CountUp>
+                +
+              </h1>
+
               <h6 className="innerfont">campus</h6>
             </div>
             <div className="col-4 headercard">
               <img src={GradIcon} alt="Graduate" />
               {/* <h1 className="headercardtext">50K+</h1> */}
-              <h1 className="headercardtext"><CountUp start={0} end={50} className="headercardtext" duration={0.9} smooth={true} smartEasingAmount={true} delay={0}></CountUp>K+</h1>
+              <h1 className="headercardtext">
+                <CountUp
+                  start={0}
+                  end={50}
+                  className="headercardtext"
+                  duration={0.9}
+                  smooth={true}
+                  smartEasingAmount={true}
+                  delay={0}
+                ></CountUp>
+                K+
+              </h1>
 
               <h6 className="innerfont">Students</h6>
             </div>
             <div className="col-4 headercard">
               <img src={BuildingIcon} alt="Building" />
               {/* <h1 className="headercardtext">18+</h1> */}
-              <h1 className="headercardtext"><CountUp start={0} end={18} className="headercardtext" duration={1} smooth={true} smartEasingAmount={true} delay={0}></CountUp>+</h1>
+              <h1 className="headercardtext">
+                <CountUp
+                  start={0}
+                  end={18}
+                  className="headercardtext"
+                  duration={1}
+                  smooth={true}
+                  smartEasingAmount={true}
+                  delay={0}
+                ></CountUp>
+                +
+              </h1>
 
               <h6 className="innerfont">Industries</h6>
             </div>
@@ -91,40 +113,37 @@ export default function NavBar() {
 
       <div className="row Studentsfrom">
         <div className="studenttxt">Our students comes from</div>
-
-        <Swiper className="row cllgCarol">
+        <div>
           <div className="col-lg-2 studentfrom">IIT Kanpur</div>
           <div className="col-lg-2 studentfrom">IIT Roorkee</div>
           <div className="col-lg-2 studentfrom">NIT Delhi</div>
           <div className="col-lg-2 studentfrom">IIIT Vadodra</div>
           <div className="col-lg-2 studentfrom">IIT Bombay</div>
-        </Swiper>
+        </div>
       </div>
 
       <div className="row Studentsfrom2">
         <div className="studenttxt">Companies we Collaborate</div>
 
         <div className="row">
-          <Swiper {...params}>
-            <div className="comp studentfrom2">
-              <img src={tcs} alt="tcs" className="tcsimage" />
-            </div>
-            <div className="comp studentfrom2">
-              <img src={swiggy} alt="swiggy" className="tcsimage" />
-            </div>
-            <div className="comp studentfrom2">
-              <img src={oracle} alt="oracle" className="tcsimage" />
-            </div>
-            <div className="comp studentfrom2">
-              <img src={logitech} alt="logitech" className="tcsimage" />
-            </div>
-            <div className="comp studentfrom2">
-              <img src={infosys} alt="infosys" className="tcsimage" />
-            </div>
-            <div className="comp studentfrom2">
-              <img src={vedantu} alt="vedantu" className="tcsimage" />
-            </div>
-          </Swiper>
+          <div className="comp studentfrom2">
+            <img src={tcs} alt="tcs" className="tcsimage" />
+          </div>
+          <div className="comp studentfrom2">
+            <img src={swiggy} alt="swiggy" className="tcsimage" />
+          </div>
+          <div className="comp studentfrom2">
+            <img src={oracle} alt="oracle" className="tcsimage" />
+          </div>
+          <div className="comp studentfrom2">
+            <img src={logitech} alt="logitech" className="tcsimage" />
+          </div>
+          <div className="comp studentfrom2">
+            <img src={infosys} alt="infosys" className="tcsimage" />
+          </div>
+          <div className="comp studentfrom2">
+            <img src={vedantu} alt="vedantu" className="tcsimage" />
+          </div>
         </div>
       </div>
 
