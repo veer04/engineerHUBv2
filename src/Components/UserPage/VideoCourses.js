12a0ead07a2e1e24c9630155ac6b3f587e7c6fa6
Aditi Jaiswal <jaisaditi19@gmail.com>
2022-09-27 13:21:38 +0530

    video updated

--- src/Components/UserPage/VideoCourses.js
+++ src/Components/UserPage/VideoCourses.js
@@ -4,13 +4,16 @@ import ReactPlayer from "react-player";
 const VideoCourses = () => {
   return (
     <div>
+      <div className="video-wrapper">
     <ReactPlayer
       url="https://www.youtube.com/watch?v=B-ytMSuwbf8"
-      className="react_player"
+      className="react-player"
       controls={true}
-      width="65vw"
-      height="70vh"
+      style={{padding:"5%"}}
+      width="100%"
+      height="100%"
     />
+    </div>
     <div className="video-name">Introduction to Video Courses</div>
     <div className="video-desc">Learn about what Intermediate JavaScript has in store!</div>
     </div>
