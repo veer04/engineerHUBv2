210726f10a8f22c520890a3d90492a8752abcbc5
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2023-04-13 19:05:32 +0530

    changed folder structure

--- src/features/DeCrypt.jsx
+++ src/features/DeCrypt.jsx
@@ -1,10 +1,9 @@
-// require("dotenv").config();
 import { AES, enc } from "crypto-js";
-const secretKey = import.meta.env.VITE_REACT_APP_AESKEY;
+import { AESKEY } from "../services/APIUtils";
 
 const decryptData = (encryptedData) => {
   const decryptedData = JSON.parse(
-    AES.decrypt(encryptedData, secretKey).toString(enc.Utf8)
+    AES.decrypt(encryptedData, AESKEY).toString(enc.Utf8)
   );
   return decryptedData;
 };
