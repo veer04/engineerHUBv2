e5c630a191bec5aa68ea4867fda6f808924a13e3
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2023-04-19 23:46:26 +0530

    completed chat section

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import mentor from "./svg/mentor.svg";
 import submit from "./svg/submit.svg";
 import Message from "./Message";
 
 export default function Chat({ className }) {
+  const [messages, setMessages] = useState([
+    {
+      _id: 1,
+      userId: 1001,
+      userName: "Manish Rai",
+      isVerified: true,
+      time: "12:00 PM",
+      tags: ["Mentor"],
+      message: "Lorem ipsum dolor sit amet",
+      avatar: "https://source.unsplash.com/random/",
+    },
+    {
+      _id: 2,
+      userId: 1002,
+      userName: "Yash Vardhan",
+      isVerified: false,
+      time: "12:15 PM",
+      message:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam debitis porro velit dicta, nesciunt aperiam sequi consectetur eum, dignissimos obcaecati eos voluptatibus blanditiis impedit expedita suscipit similique ea doloremque.",
+      avatar: "https://source.unsplash.com/random/",
+    },
+    {
+      _id: 3,
+      userId: 1003,
+      userName: "Ayush Gupta",
+      isVerified: true,
+      time: "12:30 PM",
+      tags: ["Mentor", "Head"],
+      message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      avatar: "https://source.unsplash.com/random/",
+    },
+    {
+      _id: 4,
+      userId: 2001,
+      userName: "Swapnil Raj",
+      time: "12:45 PM",
+      message:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet",
+      avatar: "https://source.unsplash.com/random/",
+    },
+  ]);
+
+  const renderedMessages = messages.map((message) => {
+    return <Message key={message._id} {...message} />;
+  });
+
+  function handleSubmit() {
+    const input = document.getElementById("chat-input");
+    if (input.value) {
+      setMessages([
+        ...messages,
+        {
+          _id: messages.length + 1,
+          userId: 2001,
+          userName: "Swapnil Raj",
+          time: "12:45 PM",
+          message: input.value,
+          avatar: "https://source.unsplash.com/random/",
+        },
+      ]);
+      input.value = "";
+    }
+    document.getElementByClasses("chat-display").scrollTo(0, 999999999);
+  }
+
   return (
     <div className={`chat-container ${className ? className : ""}`}>
       <div className="chat-header">
@@ -13,13 +78,16 @@ export default function Chat({ className }) {
           <img src={mentor} alt="Connect to mentor" />
         </div>
       </div>
-      <div className="chat-display">
-        <Message />
-      </div>
+      <div className="chat-display">{renderedMessages}</div>
       <div className="chat-input">
-        <input className="input" placeholder="New Message" type="text" />
+        <input
+          id="chat-input"
+          className="input"
+          placeholder="New Message"
+          type="text"
+        />
         <div className="submit-button__container">
-          <div className="submit-button">
+          <div onClick={handleSubmit} className="submit-button">
             <img src={submit} alt="Submit" />
           </div>
         </div>
--- src/Components/Chat/Message.jsx
+++ src/Components/Chat/Message.jsx
@@ -1,33 +1,65 @@
 import React from "react";
 import "./Message.css";
-import verified from "./svg/verified.svg";
+import verifiedIcon from "./svg/verified.svg";
 import options from "./svg/options.svg";
 
-export default function Message() {
+export default function Message({
+  _id,
+  userId,
+  avatar,
+  userName,
+  isVerified,
+  time,
+  tags,
+  message,
+}) {
+  const MY_USER_ID = 2001;
+
+  const chatMessageClasses = `chat-message ${
+    userId === MY_USER_ID && "chat-message--flipped"
+  }`;
+
+  if (userId === 2001) {
+  }
+
+  const messageContainerClasses = `message-container ${
+    userId === MY_USER_ID && "message-container--flipped"
+  }`;
+
+  if (userId === 2001) {
+  }
+
+  const messageHeaderClasses = `message-header ${
+    userId === MY_USER_ID && "message-header--flipped"
+  }`;
+
+  const messageBodyClasses = `message-body ${
+    userId === MY_USER_ID && "message-body--flipped"
+  }`;
+
   return (
-    <div className="chat-message">
+    <div className={chatMessageClasses}>
       <div className="avatar-container">
-        <img
-          className="avatar"
-          src="https://source.unsplash.com/random/"
-          alt="avatar"
-        />
+        <img className="avatar" src={avatar} alt="avatar" />
       </div>
-      <div className="message-container">
-        <div className="message-header">
-          <div className="name">John Doe</div>
-          <img src={verified} alt="verified" />
-          <div className="time">12:00 PM</div>
+      <div className={messageContainerClasses}>
+        <div className={messageHeaderClasses}>
+          <div className="name">{userName}</div>
+          {isVerified && <img src={verifiedIcon} alt="verified" />}
+          <div className="time">{time}</div>
         </div>
         <div className="tags">
-          <div className="tag">Mentor</div>
-          <div className="tag">Head</div>
+          {tags &&
+            tags.map((tag) => {
+              return (
+                <div key={tag} className="tag">
+                  {tag}
+                </div>
+              );
+            })}
         </div>
-        <div className="message-body">
-          <div className="message">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos,
-            eveniet?
-          </div>
+        <div className={messageBodyClasses}>
+          <div className="message">{message}</div>
           <div className="options">
             <img src={options} alt="options" />
           </div>
