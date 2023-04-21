3bb3f8eb35bf11477e00690d8dd2bbc50ddf0ac3
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2023-04-21 22:01:57 +0530

    changed env variables

--- src/services/APIUtils.jsx
+++ src/services/APIUtils.jsx
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.VITE_REACT_APP_URL;
-export const Bucket_URL = import.meta.env.VITE_REACT_APP_BUCKET_URL;
-export const AESKEY = import.meta.env.VITE_REACT_APP_AESKEY;
+export const API_URL = import.meta.env.VITE_API_URL;
+export const Bucket_URL = import.meta.env.VITE_BUCKET_URL;
+export const AESKEY = import.meta.env.VITE_AESKEY;
