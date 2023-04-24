16f5179d355f5424e36672023d7cf435572ca9ee
Manish-rai-dev <raimanishkumar52@gmail.com>
2023-04-25 01:59:14 +0530

    alma page started

--- src/pages/Campus/ParticularCampus/AlumniPage.jsx
+++ src/pages/Campus/ParticularCampus/AlumniPage.jsx
@@ -1,8 +1,211 @@
-import React from "react";
 import "./AlumniPage.css";
-
+import Girl from  "./girl.jpeg";
+import Uber from "./uber.png";
 export default function AlumniPage() {
   return <div className="alumni-page">
-    Hi Buddy!!
+    <div className="container whiteBG">
+      <p className="headingAlumni">
+      Meet the India’s Best Alumnis
+      </p>
+      <p className="LoremFont">Lorem ipsum dolor sit amet consectetur. Vitae diam facilisi libero mauris mauris quam elit. Convallis nunc accumsan sit cum. Vitae diam eu enim dignissim donec ultrices dis amet ipsum.</p>
+    <div className="row mainContainer">
+      <div className="col-lg-3">
+      <img src={Girl} className="setImageHeighWidth" alt="" />
+
+      <p className="smallTextImage">
+      Campus With Batch
+      </p>
+      <p className="NameTextImage">
+      Name Surname
+      </p>
+      <p className="smallTextImage">
+        Designation
+      </p>
+      </div>
+      <div className="col-lg-3">
+      <img src={Girl}  className="setImageHeighWidth" alt="" />
+      <p className="smallTextImage">
+      Campus With Batch
+      </p>
+      <p className="NameTextImage">
+      Name Surname
+      </p>
+      <p className="smallTextImage">
+        Designation
+      </p>
+        </div>
+        <div className="col-lg-3">
+        <img src={Girl} className="setImageHeighWidth" alt="" />
+        <p className="smallTextImage">
+      Campus With Batch
+      </p>
+      <p className="NameTextImage">
+      Name Surname
+      </p>
+      <p className="smallTextImage">
+        Designation
+      </p>
+        </div>
+    </div>
+    </div>
+<div className="container">
+<p className="headingAlumni">
+Meet the Alumnis from your Campus
+      </p>
+      <p className="LoremFont">Lorem ipsum dolor sit amet consectetur. Vitae diam facilisi libero mauris mauris quam elit. Convallis nunc accumsan sit cum. Vitae diam eu enim dignissim donec ultrices dis amet ipsum.</p>
+
+
+<div className="inputSearchField">
+
+  <input type="text" 
+  placeholder="search"
+  className="inputSearchBox" />
+</div>
+</div>
+<div className="container">
+
+  <div className="row mainContainer">
+    <div className="col-lg-3"  >
+      <img src={Girl} alt=""className="setImageHeighWidth2" />
+      <div className="cardBottomer">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+    <div className="col-lg-3">
+      <img src={Girl} alt="" className="setImageHeighWidth2" />
+      <div className="cardBottomer2">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+    <div className="col-lg-3">
+      <img src={Girl} alt="" className="setImageHeighWidth2" />
+      <div className="cardBottomer3">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="row mainContainer">
+    <div className="col-lg-3"  >
+      <img src={Girl} alt=""className="setImageHeighWidth2" />
+      <div className="cardBottomer4">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+    <div className="col-lg-3">
+      <img src={Girl} alt="" className="setImageHeighWidth2" />
+      <div className="cardBottomer">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+    <div className="col-lg-3">
+      <img src={Girl} alt="" className="setImageHeighWidth2" />
+      <div className="cardBottomer2">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="row mainContainer">
+    <div className="col-lg-3"  >
+      <img src={Girl} alt=""className="setImageHeighWidth2" />
+      <div className="cardBottomer3">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+    <div className="col-lg-3">
+      <img src={Girl} alt="" className="setImageHeighWidth2" />
+      <div className="cardBottomer4">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+    <div className="col-lg-3">
+      <img src={Girl} alt="" className="setImageHeighWidth2" />
+      <div className="cardBottomer">
+        <p className="smallUpperText">
+        Batch (2015)
+        </p>
+        <p className="nameOfPerson">
+          Name Surname
+        </p>
+
+        <p className="companyTagAlumni">
+          SDE at Microsoft
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>;
 }
