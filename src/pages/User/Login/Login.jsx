9904dc46f1d19c26879f22c452850a2c21290fdb
Manish-rai-dev <raimanishkumar52@gmail.com>
2023-04-28 04:00:50 +0530

    In login user Registration UI created as per schema

--- src/pages/User/Login/Login.jsx
+++ src/pages/User/Login/Login.jsx
@@ -1,19 +1,29 @@
 import "./Login.css";
 import { useState, useEffect } from 'react';
 import { TextField, Button, Box } from '@mui/material';
-import GroupAddIcon from '@material-ui/icons/GroupAdd';
+// import GroupAddIcon from '@material-ui/icons/GroupAdd';
 
 const Login = () => {
+    const [values, setValues] = useState([]);
+
     const [formData, setFormData] = useState({
         name: '',
         userName: '',
         email: '',
         mobile: '',
         college: '',
-        contact: '',
+        branch: '',
+        institutionName:'',
+        city:'',
         country: '',
+        password:'',
+        conPassword:'',
+        values:[],
       });
-    
+      const handleInputChange = (event) => {
+        const inputValues = event.target.value.split(',');
+        setValues(inputValues);
+      };
       const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData((prevFormData) => ({
@@ -33,8 +43,62 @@ const Login = () => {
     <div className="Login">
         <div className="container">
             <div className="row">
-                <div className="col-lg-4"></div>
-                <div className="col-lg-8">
+                <div className="col-lg-3 sideMenuLogin">
+    <p className="sidemenuBarHeaderLogin">
+        For Users
+
+    </p>
+    <div className="formSideMenuBar">
+        <div className="sideMenuList">
+        Registraions
+        </div>
+        <div className="sideMenuList">
+        Watchlist
+        </div>
+        <div className="sideMenuList">
+        Recently viewed
+        </div>
+        <div className="sideMenuList">
+        Mentor Sessions
+        </div>
+        <div className="sideMenuList">
+        Courses
+        </div>
+        <div className="sideMenuList">
+        Liked domains
+        </div>
+        <div className="sideMenuList">
+        Prizes/Rewards
+        </div>
+        <div className="sideMenuList">
+       Notifications
+        </div>
+    </div>
+    <p className="sidemenuBarHeaderLogin">
+        For Organizations
+
+    </p>
+    <div className="formSideMenuBar">
+    <div className="sideMenuList">
+       Manage Lists
+        </div>
+         <div className="sideMenuList">
+       My Events
+        </div> 
+       
+    </div>
+    <p className="sidemenuBarHeaderLogin">
+        For Mentors
+
+    </p>
+    <div className="formSideMenuBar">
+    <div className="sideMenuList">
+      Mentor Profile
+        </div>
+    
+    </div>
+                </div>
+                <div className="col-lg-9">
                     <div className="form-container">
                         <p className="LformHeaderText">Basic Details</p>
                         <Box component="form" onSubmit={handleSubmit}>
@@ -84,10 +148,19 @@ const Login = () => {
         margin="normal"
       />
       <TextField
-        name="contact"
-        label="Contact No."
+        name="branch"
+        label="branch No."
         variant="outlined"
-        value={formData.contact}
+        value={formData.branch}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        name="city"
+        label="city"
+        variant="outlined"
+        value={formData.city}
         onChange={handleChange}
         fullWidth
         margin="normal"
@@ -101,6 +174,40 @@ const Login = () => {
         fullWidth
         margin="normal"
       />
+       <TextField
+        name="institutionName"
+        label="institutionName"
+        variant="outlined"
+        value={formData.institutionName}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+      />
+       <TextField
+        name="password"
+        label="password"
+        variant="outlined"
+        value={formData.password}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+      />
+       <TextField
+        name="conPassword"
+        label="conPassword"
+        variant="outlined"
+        value={formData.conPassword}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+      />
+       <input type="text" onChange={handleInputChange} />
+      <ul>
+        {values.map((value, index) => (
+          <li key={index}>{value.trim()}</li>
+        ))}
+      </ul>
+
       <Button type="submit" variant="contained" color="primary">
         Submit
       </Button>
