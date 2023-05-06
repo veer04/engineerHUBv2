fb5e78c1d13bf95aa8f375575f89646b4e3f19d4
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-05-06 17:15:42 +0530

    changed domains coloring

--- src/Components/Domains/Domains.jsx
+++ src/Components/Domains/Domains.jsx
@@ -3,17 +3,20 @@ import "./Domains.css";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
 export default function Domains({ domains }) {
+  const colors = ["#F7D77F", "#8FC8E8", "#B2E887", "#E8BA98"];
+
   return (
     <div className="domains-section">
       <div className="domains-section__list">
-        {domains.map((item) => (
+        {domains.map((item, index) => (
           <Link key={item._id} to={`/community/projects/${item.domain}`}>
             <div style={{ position: "relative", borderRadius: ".6rem" }}>
               <div
                 style={{
                   backgroundImage: `url(${item.domainImage})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
+                  backgroundColor: colors[index % colors.length],
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "bottom right",
                 }}
                 className="domains-section__item dynamicShadow--off"
               >
