27871fbfd86c733d43736fca56499bf8809ac89b
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-05-15 22:14:04 +0530

    fixed blogs window bug

--- src/components/BlogWindow/BlogWindow.jsx
+++ src/components/BlogWindow/BlogWindow.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./BlogWindow.css";
 import { RxCross1 } from "react-icons/rx";
-import { GiCancel} from "react-icons/gi"
+import { GiCancel } from "react-icons/gi";
 import { getBlogById } from "../../services/APIConfig";
 import useSidebar from "../../hooks/use-sidebar";
 import Cancel from "./cancel.svg";
@@ -20,7 +20,7 @@ export default function BlogWindow({ blogOpened, setIsBlogOpen }) {
   useEffect(() => {
     console.log(blog);
   }, [blog]);
-//a dummy commit
+
   const date = blog.createdAt ? new Date(blog.createdAt) : new Date();
   return (
     <div className="project__window">
@@ -29,7 +29,7 @@ export default function BlogWindow({ blogOpened, setIsBlogOpen }) {
           <div className="title">{blog.title}</div>
         </div>
         <div onClick={() => setIsBlogOpen(false)} className="link">
-          <RxCross1/>
+          <RxCross1 />
         </div>
       </div>
       <div
@@ -50,8 +50,8 @@ export default function BlogWindow({ blogOpened, setIsBlogOpen }) {
         <div className="description">{blog.postArea}</div>
       </div>
       <div className="blog__window__details">
-        {blog.creator && (
-          <div className="author">{`by ${blog.creator[0].name}`}</div>
+        {blog.creatorId && (
+          <div className="author">{`by ${blog.creatorId.name}`}</div>
         )}
         <div className="date">
           {blog.createdAt &&
