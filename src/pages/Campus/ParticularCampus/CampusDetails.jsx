89d300d373b9334df6d149fbaca30b2be258bd2a
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-05-25 22:58:03 +0530

    integrated campus details page

--- src/pages/Campus/ParticularCampus/CampusDetails.jsx
+++ src/pages/Campus/ParticularCampus/CampusDetails.jsx
@@ -77,6 +77,19 @@ export default function CampusDetails({ path }) {
     },
   ];
 
+  //function to filter courses name from campus data
+  const filterCourses = (campus) => {
+    let courses = [];
+    if (campus.feesCourses) {
+      campus.feesCourses.forEach((course) => {
+        //add only unique courses
+        if (!courses.includes(course.courseField))
+          courses.push(course.courseField);
+      });
+    }
+    return courses;
+  };
+
   return (
     <div className="campus-details-page">
       {/* <div className="category-bar-container"> */}
@@ -101,64 +114,67 @@ export default function CampusDetails({ path }) {
         <div className="heading">Courses & Fees</div>
         <div className="subheading">Explore Courses</div>
         <div className="courses">
-          {courseDetails.map((course) => (
-            <div key={course._id} className="course">
-              {course.name}
+          {filterCourses(campus).map((course) => (
+            <div key={course} className="course">
+              {course}
             </div>
           ))}
         </div>
         <div className="subheading">Explore Fees Structure</div>
         <div className="course-structure__container">
-          {courseDetails.map((course) => (
-            <div key={course._id} className="course-structure">
-              <div className="row-1">
-                <div className="title">{course.longName}</div>
-                <a href={course.link} target="_blank">
-                  <div className="brochure">
-                    <svg
-                      width="15"
-                      height="15"
-                      viewBox="0 0 15 15"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M14 9.66797V12.5569C14 12.9399 13.8478 13.3073 13.5769 13.5782C13.306 13.8491 12.9386 14.0013 12.5556 14.0013H2.44444C2.06135 14.0013 1.69395 13.8491 1.42307 13.5782C1.15218 13.3073 1 12.9399 1 12.5569V9.66797"
-                        stroke="white"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M3.88867 6.05469L7.49978 9.6658L11.1109 6.05469"
-                        stroke="white"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M7.5 9.66667V1"
-                        stroke="white"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                    Brochure
+          {campus &&
+            campus.feesCourses.map((course) => (
+              <div key={course._id} className="course-structure">
+                <div className="row-1">
+                  <div className="title">
+                    {course.branch} in {course.courseField}
                   </div>
-                </a>
+                  <a href={course.Brochure} target="_blank">
+                    <div className="brochure">
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M14 9.66797V12.5569C14 12.9399 13.8478 13.3073 13.5769 13.5782C13.306 13.8491 12.9386 14.0013 12.5556 14.0013H2.44444C2.06135 14.0013 1.69395 13.8491 1.42307 13.5782C1.15218 13.3073 1 12.9399 1 12.5569V9.66797"
+                          stroke="white"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M3.88867 6.05469L7.49978 9.6658L11.1109 6.05469"
+                          stroke="white"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M7.5 9.66667V1"
+                          stroke="white"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      Brochure
+                    </div>
+                  </a>
+                </div>
+                <div className="row-2">
+                  <div>Duration: {course.courseDuration}</div>
+                  <div>Fees: {course.totalFees}</div>
+                  <div>Seats: {course.availableSeat}</div>
+                </div>
               </div>
-              <div className="row-2">
-                <div>Duration: {course.duration}</div>
-                <div>Fees: {course.fees}</div>
-                <div>Seats: {course.seats}</div>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
 
-      <div className="cutoff">
+      {/* <div className="cutoff">
         <div className="heading">Cut-offs</div>
         <div className="selection">
           <div>
@@ -170,24 +186,16 @@ export default function CampusDetails({ path }) {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Engineering and Architecture
+                Select Domain
               </button>
               <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
+                {filterCourses(campus).map((course) => (
+                  <li key={course}>
+                    <a className="dropdown-item" href="#">
+                      {course}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
@@ -249,7 +257,7 @@ export default function CampusDetails({ path }) {
             </tr>
           </tbody>
         </table>
-      </div>
+      </div> */}
     </div>
   );
 }
