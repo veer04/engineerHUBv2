93d658d61e0f02046e601a25f8992938c90daad4
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-05-28 04:36:27 +0530

    integrated post modal

--- src/components/PostModal/PostModal.jsx
+++ src/components/PostModal/PostModal.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 import "./PostModal.css";
-import { MdCancel } from "react-icons/md";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import {
-  controller,
-  getParticularEvent,
-  getPostById,
-} from "../../services/APIConfig";
+import { controller, getPostById } from "../../services/APIConfig";
 import { useState } from "react";
 import { FaRegHeart } from "react-icons/fa";
 import { GrShareOption } from "react-icons/gr";
+import { RWebShare } from "react-web-share";
+import { FRONTEND_URL } from "../../services/APIUtils";
 
-export default function PostModal({ handleClose, setShowModal }) {
-  const { postId } = useParams();
+export default function PostModal() {
+  const { postId, collegeId, clubId } = useParams();
+
   const [post, setPost] = useState(
     sessionStorage.getItem(`post details ${postId}`)
       ? JSON.parse(sessionStorage.getItem(`post details ${postId}`))
       : {}
   );
+  const [isLiked, setIsLiked] = useState(
+    sessionStorage.getItem(`${postId} isLiked`) !== null
+      ? JSON.parse(sessionStorage.getItem(`${postId} isLiked`))
+      : false
+  );
   const navigate = useNavigate();
+
+  //function to find out window current scroll position
+  function getScrollPosition() {
+    var doc = document.documentElement;
+    var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+    var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    return { top, left };
+  }
+
   useEffect(() => {
     document.body.classList.add("overflow-hidden");
 
@@ -43,45 +55,74 @@ export default function PostModal({ handleClose, setShowModal }) {
   }, [post]);
 
   return ReactDOM.createPortal(
-    <div id="post-modal-container">
-      <div className="post-container">
-        <div
-          style={{
-            backgroundImage: `url(${post.postLogo})`,
-          }}
-          className="post-image"
-        ></div>
-        <div className="post-details-container">
-          <div className="club-details">
-            <div
-              style={{
-                backgroundImage: `url(${post.clubId.image})`,
-              }}
-              className="club-logo"
-            ></div>
-            <div className="club-name text-crop-1">{post.clubId.name}</div>
-          </div>
-          <div className="description">{post.description}</div>
-          <div className="options-container">
-            <div className="likes">Liked by {post.likes}</div>
-            <div className="options">
-              <div className="like">
-                <FaRegHeart />
+    <div
+      style={{
+        top: getScrollPosition().top,
+        left: getScrollPosition().left,
+      }}
+      id="post-modal-container"
+    >
+      {post.description ? (
+        <div className="post-container">
+          <div
+            style={{
+              backgroundImage: `url(${post.postLogo})`,
+            }}
+            className="post-image"
+          ></div>
+          <div className="post-details-container">
+            <div className="club-details">
+              <div
+                style={{
+                  backgroundImage: `url(${post.clubId.image})`,
+                }}
+                className="club-logo"
+              ></div>
+              <div className="club-name text-crop-1">{post.clubId.name}</div>
+            </div>
+            <div className="description">{post.description}</div>
+            <div className="options-container">
+              <div className="likes">
+                Liked by {isLiked ? post.likes + 1 : post.likes}
               </div>
-              <div className="share">
-                <GrShareOption />
+              <div className="options">
+                <div
+                  onClick={() => {
+                    const currentStatus = isLiked;
+                    setIsLiked(!currentStatus);
+                    sessionStorage.setItem(
+                      `${_id} isLiked`,
+                      JSON.stringify(!currentStatus)
+                    );
+                  }}
+                  style={{
+                    backgroundColor: isLiked ? "#fec2cb" : "",
+                  }}
+                  className="like"
+                >
+                  <FaRegHeart />
+                </div>
+                <div className="share">
+                  <RWebShare
+                    data={{
+                      text: `Check out this post`,
+                      url: `${FRONTEND_URL}campus/${collegeId}/technical-clubs/${clubId}/posts/${postId}}`,
+                      title: "Check out this post at engineerHUB",
+                    }}
+                    onClick={() => console.info("shared successfully")}
+                  >
+                    <GrShareOption />
+                  </RWebShare>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* {event.eventName ? (
-        <></>
       ) : (
         <div className="spinner-border text-light" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
-      )} */}
+      )}
     </div>,
     document.querySelector("#post-modal")
   );
