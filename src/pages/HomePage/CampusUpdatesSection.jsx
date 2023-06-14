ef57f0a38d07acd786dc55499d7716df692fd040
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-06-14 12:28:04 +0530

    added campus updates

--- src/pages/HomePage/CampusUpdatesSection.jsx
+++ src/pages/HomePage/CampusUpdatesSection.jsx
@@ -9,23 +9,26 @@ export default function CampusUpdatesSection() {
   //fetch updates from backend
   const updates = [
     {
-      title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      title: "• IIT Madras is going to have a IOT WORKSHOP on 17.6.23",
     },
     {
-      title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      title:
+        "• IIT  kharagpur is going to have a online workshop of MATLAB Programming for Physical & Chemical Sciences from 18th june 2023-3rd july 2023 (3weekends)",
+    },
+    // {
+    //   title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // },
+    {
+      title:
+        "• Hybrid workshop on scale development and impactful publication in social science to be held w.e.f. 19 to 23 July 2023 at NIT Hamirpur",
     },
     {
-      title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      title:
+        "• Understanding of molecular simulation at NIT Rourkela form 19 to 23 June 2023",
     },
-    {
-      title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
+    // {
+    //   title: "• Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // },
   ];
 
   //fetch outerUpdates from backend
@@ -45,13 +48,13 @@ export default function CampusUpdatesSection() {
       <div className="campus-update-card">
         <div className="campus-update-title">{updates[0].title}</div>
         <div className="campus-update-title">{updates[1].title}</div>
-        <div className="campus-update-title">{updates[2].title}</div>
+        {/* <div className="campus-update-title">{updates[2].title}</div> */}
       </div>
     );
   }
 
-  const updatesContainer1 = updatesCard(updates.slice(0, 3));
-  const updatesContainer2 = updatesCard(updates.slice(3, 6));
+  const updatesContainer1 = updatesCard(updates.slice(0, 2));
+  const updatesContainer2 = updatesCard(updates.slice(2, 4));
 
   return (
     <div className="campus-section-container">
