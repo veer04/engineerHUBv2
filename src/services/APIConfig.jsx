5fc38d0c909784bcfe278735f3857c1f3c90fa83
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-06-14 11:46:47 +0530

    removed wrong all events api

--- src/services/APIConfig.jsx
+++ src/services/APIConfig.jsx
@@ -576,25 +576,6 @@ export const getEvents = (setEvents, id) => {
     });
 };
 
-export const getEventById = (setEvent) => {
-  const controller = new AbortController();
-  axios
-    .get(`${API_URL}api/v1/event`, {
-      signal: controller.signal,
-    })
-    .then((res) => {
-      const data = res.data.data;
-      setEvent(data);
-    })
-    .catch((err) => {
-      if (axios.isCancel(err)) {
-        console.log("req cancel");
-      } else {
-        console.log("req performed");
-      }
-    });
-};
-
 export const getParticularEvent = (setEvent, eventId) => {
   const controller = new AbortController();
   axios
