b3cfab93bf9cd0ed5e1ee157b8aab74fa0d7b2a0
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-06-14 11:52:48 +0530

    added featured events api to other places too

--- src/pages/HomePage/ReviewsSection.jsx
+++ src/pages/HomePage/ReviewsSection.jsx
@@ -9,9 +9,6 @@ export default function ReviewsSection() {
   useEffect(() => {
     getReviews(setReviews);
   }, []);
-  useEffect(() => {
-    console.log(reviews);
-  }, [reviews]);
   let randomnessFactor = 15;
   const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
