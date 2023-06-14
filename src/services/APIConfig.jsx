23670d09c5a034188e0e9795ce6e86dca907c871
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-06-14 11:44:27 +0530

    added featured events api to homepage

--- src/services/APIConfig.jsx
+++ src/services/APIConfig.jsx
@@ -538,6 +538,25 @@ export const getAllEvents = (setEvents) => {
     });
 };
 
+export const getFeaturedEvents = (setFeaturedEvents) => {
+  const controller = new AbortController();
+  axios
+    .get(`${API_URL}api/v1/featuredEvent`, {
+      signal: controller.signal,
+    })
+    .then((res) => {
+      const data = res.data.data;
+      setFeaturedEvents(data);
+    })
+    .catch((err) => {
+      if (axios.isCancel(err)) {
+        console.log("req cancel");
+      } else {
+        console.log("req performed");
+      }
+    });
+};
+
 export const getEvents = (setEvents, id) => {
   const controller = new AbortController();
   axios
