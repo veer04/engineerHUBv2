7fbad26110d56136efe9786bcad88fa4570f996b
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-06-19 23:01:57 +0530

    removed caption from alumni page

--- src/pages/Campus/ParticularCampus/AlumniPage.jsx
+++ src/pages/Campus/ParticularCampus/AlumniPage.jsx
@@ -38,11 +38,6 @@ export default function AlumniPage() {
     <div className="alumni-page">
       <div className="best-alumni-container">
         <p className="heading">Meet the India’s Best Alumnis</p>
-        <p className="description">
-          Lorem ipsum dolor sit amet consectetur. Vitae diam facilisi libero
-          mauris mauris quam elit. Convallis nunc accumsan sit cum. Vitae diam
-          eu enim dignissim donec ultrices dis amet ipsum.
-        </p>
         <div className="best-alumni">
           {bestAlumni.map((alumni) => (
             <AlumniGlobalCard key={alumni._id} {...alumni} />
@@ -51,11 +46,6 @@ export default function AlumniPage() {
       </div>
       <div className="campus-alumni-container">
         <p className="heading">Meet the Alumnis from your Campus</p>
-        <p className="description">
-          Lorem ipsum dolor sit amet consectetur. Vitae diam facilisi libero
-          mauris mauris quam elit. Convallis nunc accumsan sit cum. Vitae diam
-          eu enim dignissim donec ultrices dis amet ipsum.
-        </p>
         {/* <div className="search-bar">
       <input type="text" placeholder="Search" />
     </div> */}
