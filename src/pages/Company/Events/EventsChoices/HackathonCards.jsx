4f7b002a1b6ab3a5d4d54341a9592bab5478c17f
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-08-03 19:38:21 +0530

    number data on event cards on company page made random

--- src/pages/Company/Events/EventsChoices/HackathonCards.jsx
+++ src/pages/Company/Events/EventsChoices/HackathonCards.jsx
@@ -4,12 +4,16 @@ import { BsStar } from "react-icons/bs";
 import { CgEye } from "react-icons/cg";
 import { Chip } from "@mui/material";
 import "./HackathonCards.css";
-import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
-import { controller, getHiringDataById } from "../../../../services/APIConfig";
+import { useNavigate } from "react-router-dom";
 
-const HackathonCard = ({ details, color, data }) => {
+const HackathonCard = ({
+  _id,
+  opportunityPoster,
+  organisationLogo,
+  opportunityName,
+  opportunityLocation,
+  skillsRequired,
+}) => {
   // const hiringId=useParams();
   // const[eventData,setEventData]=useState({})
   // useEffect(()=>
@@ -20,24 +24,36 @@ const HackathonCard = ({ details, color, data }) => {
   //   }
   // })
   const navigate = useNavigate();
+
+  //data should be a object containing stars,views and days
+  // stars should be random number between 3 and 5 using a random function and _id as its seed
+  // views should be random number between 100 and 1000 using a random function and _id as its seed
+  // days should be random number between 5 and 30 using a random function and _id as its seed
+  let generate_random_number = new Math.seedrandom(_id);
+  const data = {
+    stars: Math.floor(generate_random_number() * (5 - 3 + 1) + 3),
+    views: Math.floor(generate_random_number() * (1000 - 100 + 1) + 100),
+    days: Math.floor(generate_random_number() * (30 - 5 + 1) + 5),
+  };
+
   return (
     <div
       onClick={() => {
-        navigate(`/company/events/${details._id}`);
+        navigate(`/company/events/${_id}`);
       }}
       className="HackathonCard"
     >
       <div className="cardImg">
-        <img src={details.opportunityPoster} alt="" />
+        <img src={opportunityPoster} alt="" />
         <span className="GoogleIcon">
-          <img src={details.organisationLogo} alt="Logo" />
+          <img src={organisationLogo} alt="Logo" />
         </span>
       </div>
       <div className="cardBody">
-        <h4>{details.opportunityName}</h4>
-        <h6>{details.opportunityLocation}</h6>
+        <h4>{opportunityName}</h4>
+        <h6>{opportunityLocation}</h6>
         <span className="Tags">
-          {details.skillsRequired?.map((tag, index) => (
+          {skillsRequired?.map((tag, index) => (
             <Chip
               key={index}
               variant="outlined"
