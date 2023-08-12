6758d2d36669d3308c9e1de1c6b9b85cef15ddc3
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-08-12 19:19:52 +0530

    PD-8 added search bar to inner pages

--- src/pages/Company/Events/EventsChoices/HackathonDetails.jsx
+++ src/pages/Company/Events/EventsChoices/HackathonDetails.jsx
@@ -1,77 +1,22 @@
-import React from "react";
-import { AiOutlineClockCircle } from "react-icons/ai";
-import { BsStar } from "react-icons/bs";
-import { CgEye } from "react-icons/cg";
-import { Chip } from "@mui/material";
+import { useMemo } from "react";
 import "./HackathonDetails.css";
 import HackathonDesc from "./HackathonDesc";
 import { useParams } from "react-router-dom";
-import { Bucket_URL } from "../../../../services/APIUtils";
 import { useEffect, useState } from "react";
-import colorWheel from "../../../../assets/colorWheel";
 import {
   controller,
   getHiringData,
   getHiringDataById,
 } from "../../../../services/APIConfig";
 import HackathonCard from "./HackathonCards";
-const Card = ({ details }) => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [window.location.pathname]);
-  const HackathonDummyData = {};
-  return (
-    <div
-      className="Card"
-      onClick={() => {
-        navigate(`/company/events/${details._id}`);
-      }}
-    >
-      <div className="cardImg">
-        <img src={details.opportunityPoster} alt="" />
-        <span className="GoogleIcon">
-          <img src={details.organisationLogo} alt="Logo" />
-        </span>
-      </div>
-      <div className="cardBody">
-        <h4>{details.opportunityName}</h4>
-        <h6>{details.opportunityLocation}</h6>
-        <span className="Tags">
-          {details?.skillsRequired.map((tag, index) => (
-            <Chip
-              key={index}
-              variant="outlined"
-              size="small"
-              label={tag}
-              style={{
-                fontWeight: "500",
-                fontSize: "10px",
-                marginRight: "15px",
-              }}
-            />
-          ))}
-        </span>
-        <div className="Stats">
-          <span>
-            <BsStar /> {4}
-          </span>
-          <span>|</span>
-          <span>
-            <CgEye /> {1000} Views
-          </span>
-          <span>|</span>
-          <span>
-            <AiOutlineClockCircle /> {14} Days Left
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const HackathonDetails = () => {
   const [hiring, setHiring] = useState([]);
   const [hiringData, setHiringData] = useState({});
+  const [query, setQuery] = useState("");
+  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [searchedProjects, setSearchedProjects] = useState([]);
+
   const { hackId } = useParams();
   useEffect(() => {
     getHiringData(setHiring);
@@ -80,45 +25,70 @@ const HackathonDetails = () => {
       controller.abort();
     };
   }, [hackId]);
-  const bucket = `${Bucket_URL}frontend/company/events/hackathon/`;
 
-  const data = [
-    {
-      stars: 3,
-      views: 426,
-      days: 9,
-    },
-    {
-      stars: 3,
-      views: 575,
-      days: 20,
-    },
-    {
-      stars: 4,
-      views: 978,
-      days: 7,
-    },
-    {
-      stars: 5,
-      views: 148,
-      days: 171,
-    },
-    {
-      stars: 3,
-      views: 429,
-      days: 15,
-    },
-    {
-      stars: 5,
-      views: 292,
-      days: 27,
-    },
-  ];
+  useEffect(() => {
+    if (searchedProjects.length > 0) {
+      setFilteredProjects(searchedProjects);
+    } else {
+      setFilteredProjects([]);
+    }
+  }, [searchedProjects]);
+
+  const filteredData = useMemo(() => {
+    return hiring.filter((value) => {
+      return (
+        value.opportunityName?.toLowerCase().includes(query.toLowerCase()) ||
+        value.amount?.toLowerCase().includes(query.toLowerCase()) ||
+        value.opportunityLocation
+          ?.toLowerCase()
+          .includes(query.toLowerCase()) ||
+        value.domainName?.toLowerCase().includes(query.toLowerCase()) ||
+        value.skillsRequired?.some((tag) =>
+          tag.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    });
+  }, [hiring, query]);
+
+  useEffect(() => {
+    setSearchedProjects(filteredData);
+  }, [query, filteredData]);
 
   return (
     <div className="HackathonDetails">
-      <div className="hackathonTiles">
-        {hiring
+      <div className="hackathonTiles job-tiles-container">
+        <div className="project__searchbar__container company_searchbar_container__aside">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+
+            <span className="input-group-text" id="basic-addon2">
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.53223 14.0332C8.92969 14.0332 10.2393 13.6113 11.3291 12.8906L15.1787 16.749C15.4336 16.9951 15.7588 17.1182 16.1104 17.1182C16.8398 17.1182 17.376 16.5469 17.376 15.8262C17.376 15.4922 17.2617 15.167 17.0156 14.9209L13.1924 11.0801C13.9834 9.95508 14.4492 8.59277 14.4492 7.11621C14.4492 3.31055 11.3379 0.199219 7.53223 0.199219C3.73535 0.199219 0.615234 3.31055 0.615234 7.11621C0.615234 10.9219 3.72656 14.0332 7.53223 14.0332ZM7.53223 12.1875C4.74609 12.1875 2.46094 9.90234 2.46094 7.11621C2.46094 4.33008 4.74609 2.04492 7.53223 2.04492C10.3184 2.04492 12.6035 4.33008 12.6035 7.11621C12.6035 9.90234 10.3184 12.1875 7.53223 12.1875Z"
+                  fill="#3C3C43"
+                  fillOpacity="0.6"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        {filteredProjects
           ?.filter((res) => res.opportunityType === "Event")
           .map((item, index) => {
             return <HackathonCard {...item} key={index} />;
