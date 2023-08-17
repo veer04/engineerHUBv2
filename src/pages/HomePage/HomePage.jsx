0f0a8548ebc36235f071bc93ec5001384e2a1fc4
Manish-rai-dev <raimanishkumar52@gmail.com>
2023-08-17 11:57:26 +0530

    gauth pushed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,9 +10,10 @@ import RecentActivitiesSection from "./RecentActivitiesSection";
 import ReviewsSection from "./ReviewsSection";
 import SiliconValley from "./SiliconValley";
 import JobsSection from "./JobsSection";
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
 import useNavbar from "../../hooks/use-navbar";
-
+import axios from "axios";
+import { API_URL } from "../../services/APIUtils";
 export default function HomePage() {
   const { setSelectedPageNavbar } = useNavbar();
   useEffect(() => {
@@ -20,40 +21,40 @@ export default function HomePage() {
     window.scrollTo(0, 0);
   }, []);
 
-  // const [me, setMe] = useState({});
+  const [me, setMe] = useState({});
 
-  // useEffect(() => {
-  //   async function getMe() {
-  //     try {
-  //       const response = await axios.get(`${API_URL}api/v1/auth/details`, {
-  //         withCredentials: true,
-  //       });
-  //       console.log("1");
-  //       console.log(response.data);
-  //       console.log(response.data.success);
-  //       setMe(response.data);
-  //       if(response.data.success===true)
-  //       {
-  //         Cookies.set("access_token",response.data.accessToken)
-  //         Cookies.set("userName",response.data.decodedToken.name)
-  //         Cookies.set("email",response.data.decodedToken.email)
-  //         Cookies.set("_id",response.data.decodedToken.id)
-  //         Cookies.set("image",response.data.decodedToken.picture);
-  //         console.log(response.data)
-  //       }
-  //       else
-  //       {
-  //         navigate("/login");
-  //         window.alert("No id exists with the provided mail signup Now!!!")
-  //       }
+  useEffect(() => {
+    async function getMe() {
+      try {
+        const response = await axios.get(`${API_URL}api/v1/auth/details`, {
+          withCredentials: true,
+        });
+        console.log("1");
+        console.log(response.data);
+        console.log(response.data.success);
+        setMe(response.data);
+        if(response.data.success===true)
+        {
+          Cookies.set("access_token",response.data.accessToken)
+          Cookies.set("userName",response.data.decodedToken.name)
+          Cookies.set("email",response.data.decodedToken.email)
+          Cookies.set("_id",response.data.decodedToken.id)
+          Cookies.set("image",response.data.decodedToken.picture);
+          console.log(response.data)
+        }
+        else
+        {
+          navigate("/login");
+          window.alert("No id exists with the provided mail signup Now!!!")
+        }
 
-  //     } catch (error) {
-  //       console.error("Error fetching user data:", error);
-  //     }
-  //   }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
 
-  //   getMe();
-  // }, []);
+    getMe();
+  }, []);
   return (
     <div className="homepage">
       <MainLandingSection />
