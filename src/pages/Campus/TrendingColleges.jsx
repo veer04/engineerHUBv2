967c2b152f1ed509b5eec1787fd6b500789a2cf4
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-10-17 16:39:02 +0530

    added cards on trending colleges

--- src/pages/Campus/TrendingColleges.jsx
+++ src/pages/Campus/TrendingColleges.jsx
@@ -2,19 +2,27 @@ import { useEffect, useState } from "react";
 import "./TrendingColleges.css";
 import CampusSearchBox from "../../components/CampusSearchBox/CampusSearchBox";
 import { useNavigate, useParams } from "react-router-dom";
-import { getAllCampuses, getCampusById } from "../../services/APIConfig";
+import {
+  getAllCampuses,
+  getCampusById,
+  getTrendingCampuses,
+} from "../../services/APIConfig";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import defaultPoster from "../../assets/defaultPoster";
 
 export default function TrendingColleges() {
   const { collegeId } = useParams();
   const navigate = useNavigate();
   //   const [width, setWidth] = useState(window.innerWidth);
+  const [trendingList, setTrendingList] = useState([]);
   const [campusData, setCampusData] = useState({});
   const [campus, setCampus] = useState({});
   const [allCampuses, setAllCampuses] = useState([]);
   const [output, setOutput] = useState("");
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
+    getTrendingCampuses(setTrendingList);
     getCampusById(setCampusData, collegeId);
     getAllCampuses(setAllCampuses);
 
@@ -25,6 +33,10 @@ export default function TrendingColleges() {
       //   window.removeEventListener("resize", handleResize);
     };
   }, [collegeId]);
+
+  useEffect(() => {
+    console.log(trendingList);
+  }, [trendingList]);
 
   useEffect(() => {
     if (output) {
@@ -52,7 +64,42 @@ export default function TrendingColleges() {
         </div>
       </div>
       <div className="content-container">
-        aside.
+        <aside className="column column-1">
+          <div className="list-heading">
+            <div>
+              <FaArrowTrendUp /> Trending Colleges
+            </div>
+          </div>
+          {trendingList?.map((item) => (
+            <div key={item._id} className="card">
+              <div className="poster">
+                {item?.collegePhoto?.length ? (
+                  <img src={item?.collegePhoto[0]} alt="poster" />
+                ) : (
+                  <img src={defaultPoster} alt="poster" />
+                )}
+              </div>
+              <div className="content">
+                <div className="logo">
+                  <img src={item?.collegeLogo} alt="logo" />
+                </div>
+                <div className="details">
+                  <span className="name text-crop-3">
+                    Bharati Vidyapeeth College of Engineering, Deemed to be
+                    University Navi Mumbai
+                  </span>
+                  <span className="location text-crop-2">Navi Mumbai</span>
+                  <div className="tags">
+                    <div className="club-count">5+ Clubs</div>
+                    <div className="alma-count">5+ Alumni</div>
+                    <div className="student-count">5+ Students</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </aside>
+        <section className="column column-2"></section>
       </div>
     </main>
   );
