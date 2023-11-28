3afa3ea8fd520cf7d140304c7f02dd8244aa3340
Swapnil Raj <raj.swapnil1708@gmail.com>
2023-11-28 18:45:49 +0530

    corrected default name and logo for engineerHUB

--- src/assets/defaultPoster.jsx
+++ src/assets/defaultPoster.jsx
@@ -1,8 +1,9 @@
-const defaultPoster =
-  "https://frontendehubbucket.s3.ap-south-1.amazonaws.com/frontend/global/default_poster.png";
-const defaultPosterArray = [
-  "https://frontendehubbucket.s3.ap-south-1.amazonaws.com/frontend/global/default_poster.png",
-];
+import { Bucket_URL } from "../services/APIUtils";
+
+const defaultPoster = `${Bucket_URL}frontend/global/default_poster.png`;
+const defaultPosterArray = [`${Bucket_URL}frontend/global/default_poster.png`];
+const ehubLogo = `${Bucket_URL}EHub_Logo.jpeg`;
 
 export default defaultPoster;
 export { defaultPosterArray };
+export { ehubLogo };
