86718c388eae89da3806a2d4b2145c60fac9e747
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-03-12 19:06:32 +0530

    feat: Add Number Input Field component

---
+++ src/components/FormInputs/FormInputNumber.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./FormInput.css";
+
+export default function FormInputNumber({
+  label,
+  required,
+  constraint,
+  placeholder,
+  value,
+  setValue,
+  min,
+  max,
+  step,
+  helperText,
+  className,
+  disabled,
+  caption,
+  ...rest
+}) {
+  return (
+    <div
+      {...rest}
+      className={`custom-form-input ${!!className ? className : ""}`}
+    >
+      <div className="form-input-container">
+        <label htmlFor={label} className="form-input-label">
+          {label}{" "}
+          {label && required && <span className="required-mark">*</span>}
+        </label>
+        {constraint && <span className="constraint">({constraint})</span>}
+      </div>
+      {caption && <p className="caption">{caption}</p>}
+      <input
+        type="number"
+        name={label}
+        id={label}
+        required={required}
+        min={min}
+        max={max}
+        step={step}
+        disabled={disabled}
+        className={`custom-input ${helperText ? "custom-input-error" : ""} ${
+          !!disabled ? "disabled" : ""
+        }`}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {helperText && <span className="helper-text">{helperText}</span>}
+    </div>
+  );
+}
