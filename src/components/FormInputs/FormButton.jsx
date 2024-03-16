0acdfc95e74b2fd1a2c33fc39ed4cb17902afd8b
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-03-17 00:41:57 +0530

    feat: Update form button to have loading state

--- src/components/FormInputs/FormButton.jsx
+++ src/components/FormInputs/FormButton.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./FormInput.css";
+import Loading from "../Loader/Loading";
 
-export default function FormButton({ className, children, ...rest }) {
+export default function FormButton({
+  isLoading,
+  className,
+  children,
+  ...rest
+}) {
   return (
-    <button className={`form-button ${className}`} {...rest}>
-      {children}
+    <button
+      disabled={isLoading}
+      className={`form-button ${className}`}
+      {...rest}
+    >
+      {isLoading ? <Loading /> : children}
     </button>
   );
 }
