8e8d87795de947c758e6d754ed2e9bcb1d4f7460
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-06-05 03:35:33 +0530

    fix: Update staleTime to 1 minute for JobsPage.jsx

--- src/pages/Company/Jobs/JobsPage.jsx
+++ src/pages/Company/Jobs/JobsPage.jsx
@@ -87,7 +87,7 @@ export default function JobsPage() {
           return res;
         });
     },
-    staleTime: 1000 * 60 * 5, // 5 minutes
+    staleTime: 1000 * 60 * 1, // 1 minutes
   });
   console.log(pageCount);
   useEffect(() => {
