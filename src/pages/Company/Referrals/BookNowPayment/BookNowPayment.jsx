7cdae903c7e019bff195de5ab510a244f70ce142
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-08-22 22:51:35 +0530

    chore: Update payment API URL in BookNowPayment component

--- src/pages/Company/Referrals/BookNowPayment/BookNowPayment.jsx
+++ src/pages/Company/Referrals/BookNowPayment/BookNowPayment.jsx
@@ -9,7 +9,7 @@ import ConnectCards from "../ConnectCards/ConnectCards";
 import { getAccessToken } from "../../../../features/getCookieValues";
 import axios from "axios";
 import {
-  BETA_SERVER,
+  PAYMENT_API_URL,
   FRONTEND_URL,
   REFERRAL_REDIRECT_URL,
 } from "../../../../services/APIUtils";
@@ -274,7 +274,7 @@ const BookNowPayment = () => {
 
     axios
       .post(
-        `${BETA_SERVER}/api/v1/meet-event/register/${meetingData._id}`,
+        `${PAYMENT_API_URL}/api/v1/meet-event/register/${meetingData._id}`,
         payload,
         {
           headers: {
@@ -310,7 +310,7 @@ const BookNowPayment = () => {
           if (meetingData?.price === 0) {
             await axios
               .post(
-                `${BETA_SERVER}/api/v1/meet-event/book/${data?.data?.meetRegistrationId}`,
+                `${PAYMENT_API_URL}/api/v1/meet-event/book/${data?.data?.meetRegistrationId}`,
                 {},
                 {
                   headers: {
@@ -362,7 +362,7 @@ const BookNowPayment = () => {
       const payload = {
         amount: totalPrice,
         currency: "INR",
-        callback_url: `${BETA_SERVER}referrals/book-now/payment/success`,
+        callback_url: `${PAYMENT_API_URL}referrals/book-now/payment/success`,
         callback_method: "get",
         platform: "meet",
         meetRegistrationId: meetId?.meetRegistrationId,
@@ -373,7 +373,7 @@ const BookNowPayment = () => {
       console.log(typeof parseFloat(payload.amount), "amount");
 
       const response = await axios.post(
-        `${BETA_SERVER}/api/v1/razorpay/createPaymentLink`,
+        `${PAYMENT_API_URL}/api/v1/razorpay/createPaymentLink`,
 
         payload,
         {
@@ -403,7 +403,7 @@ const BookNowPayment = () => {
       // setTimeout(() => {
       //   axios
       //     .post(
-      //       `${BETA_SERVER}/api/v1/razorpay/confirmPayment/${meetId?.meetRegistrationId}`,
+      //       `${PAYMENT_API_URL}/api/v1/razorpay/confirmPayment/${meetId?.meetRegistrationId}`,
       //       {},
       //       {
       //         headers: {
