e21f96cf98ffe39aa8e2bcde94903767d93500c5
SaifAlam <kaif0350@gmail.com>
2024-09-07 01:18:40 +0530

    Resolved the busy slot bug new resolved

--- src/pages/Company/Referrals/BookNow/BookNow.jsx
+++ src/pages/Company/Referrals/BookNow/BookNow.jsx
@@ -179,7 +179,6 @@ const BookNow = () => {
 
   // const datesArray = generateDatesForYear();
 
-  // // console.log(datesArray, "datearray");
   const totalDatePages = Math.ceil(datesArray.length / datesPerPage);
 
   const handleNextDates = () => {
@@ -225,9 +224,6 @@ const BookNow = () => {
       startTime.setHours(start[0], start[1], 0, 0);
 
       const Interval = parseInt(meetingData?.duration?.split(" ")[0], 10);
-      // console.log(meetingData, "dration");
-
-      // console.log(Interval, "Interval");
 
       const endTime = new Date();
       endTime.setHours(end[0], end[1], 0, 0);
@@ -254,10 +250,6 @@ const BookNow = () => {
 
     return times;
   };
-
-  // console.log(selectedDates, "selectedDate");
-  // console.log(selectedTime, "selectedTime");
-  // console.log(endTime, "endTime");
 
   useEffect(() => {
     if (Object.keys(meetingData).length > 0) {
@@ -304,10 +296,9 @@ const BookNow = () => {
         config
       );
 
-      // // console.log(data, "busyeventdata");
       setBusyEventData(data?.data);
     } catch (error) {
-      // console.log(error);
+      console.log("Error getting the data", error);
     }
   };
 
@@ -316,11 +307,8 @@ const BookNow = () => {
   useEffect(() => {
     // Now when the busy dates have been fetched then in the timeArray i want that to add a tag to that time as booked. The busy event data is an array of objects in format as end:"2024-08-25T13:00:00Z", start:"2024-08-25T12:00:00Z"
 
-    // // console.log("timeArray", timeArray);
-    // // console.log("busyEventData", busyEventData);
     let renderTimes = [];
     if (!!selectedDates) {
-      // // console.log(
       //   moment(new Date(`${selectedDates} 2024 8:30 PM`)).format(
       //     "YYYY[-]MM[-]DD[T]HH[:]mm[:]ss[Z]"
       //   )
@@ -338,7 +326,6 @@ const BookNow = () => {
       );
       // .map((time) => (time.isDisabled = false));
     }
-    // // console.log("Busy Events Updated", renderTimes);
     setTimeArrayCopy(
       timeArray.map((time) => {
         if (
@@ -351,8 +338,6 @@ const BookNow = () => {
       })
     );
   }, [busyEventData, selectedDates]);
-
-  // // console.log("timeArrayCopy", timeArrayCopy);
 
   useEffect(() => {
     getBusyEvent();
@@ -412,13 +397,9 @@ const BookNow = () => {
     };
 
     // Output for debugging
-    // // console.log("Start DateTime Object:", startDate);
-    // // console.log("End DateTime Object:", endDate);
 
     const startDateTimeISO = formatISOWithoutMilliseconds(startDate);
     const endDateTimeISO = formatISOWithoutMilliseconds(endDate);
-
-    // // console.log(startDateTimeISO, endDateTimeISO, "hgfd");
 
     // setSnackbarMessage("Registered  Meeting Successfully");
     // setSnackbarOpen(true);
@@ -437,11 +418,6 @@ const BookNow = () => {
     });
   };
 
-  // // console.log("totalDatePages",totalDatePages)
-  // // console.log("datesArray",datesArray)
-  // // console.log("datesPerPage",datesPerPage)
-  // // console.log("Array.from({ length: totalDatePages })",JSON.stringify(Array.from({ length: totalDatePages })))
-
   // i am re-creating the date and time slot booking part from here because previous one got too confusing
 
   const currentTime = new Date();
@@ -450,15 +426,12 @@ const BookNow = () => {
 
   // i want 12 dates including current day
   useEffect(() => {
-    console.log("Generating Dates");
     const dates = [];
     for (let i = 0; i < 12; i++) {
       const date = new Date();
-      console.log(date);
       date.setDate(currentTime.getDate() + 1 + i);
       dates.push(date);
     }
-    console.log(dates);
     setDates(dates);
   }, []);
 
@@ -467,32 +440,23 @@ const BookNow = () => {
   const [timeSlots, setTimeSlots] = useState([]);
 
   useEffect(() => {
-    console.log("Times");
     if (dates.length && selectedDates) {
       const year = new Date().getFullYear();
       const currentYearSelectedDate = `${selectedDates} ${year}`;
-      console.log(currentYearSelectedDate);
-      console.log("Generating Times");
       const timeSlots = [];
       // the time interval will depend on the duration we get from the meetingData.duration. meetingData.duration is a string in "30 Mins" format. Extract the "30" out of it and use that as the time interval
       const timeInterval = parseInt(meetingData?.duration?.split(" ")[0], 10);
-      console.log(timeInterval);
       // i want the times array to be for only the selected date. The selectedDates variable contains the date selected. The selectedDates is a string in format of "Sep 03". Create time slots according to this.
       const selectedDate = new Date(currentYearSelectedDate);
-      console.log("selectedDate", selectedDate);
       // i want to create time slots from 10:30 AM to 12:30 PM and 4:30 PM to 7:30 PM
       const morningStartTime = new Date(selectedDate);
       morningStartTime.setHours(10, 30, 0, 0);
-      console.log("morningStartTime", morningStartTime);
       const morningEndTime = new Date(selectedDate);
       morningEndTime.setHours(12, 30, 0, 0);
-      console.log("morningEndTime", morningEndTime);
       const eveningStartTime = new Date(selectedDate);
       eveningStartTime.setHours(16, 30, 0, 0);
-      console.log("eveningStartTime", eveningStartTime);
       const eveningEndTime = new Date(selectedDate);
       eveningEndTime.setHours(19, 30, 0, 0);
-      console.log("eveningEndTime", eveningEndTime);
       // now i want to create time slots from morningStartTime to morningEndTime and eveningStartTime to eveningEndTime
       morningStartTime.getTime();
       for (
@@ -507,8 +471,6 @@ const BookNow = () => {
         // const displayHours = hours % 12 || 12;
         // const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
         // const timeString = `${displayHours}:${displayMinutes} ${ampm}`;
-        console.log(time);
-        console.log(new Date(time));
         timeSlots.push({ time: time, isEventBusy: false, lastTime: lastTime });
       }
       for (
@@ -527,7 +489,6 @@ const BookNow = () => {
         timeSlots.push({ time: time, isEventBusy: false, lastTime: lastTime });
       }
 
-      console.log("timeSlots", timeSlots);
       setTimeSlots(timeSlots);
     }
   }, [dates, selectedDates]);
@@ -552,10 +513,6 @@ const BookNow = () => {
           (reqStart <= startTime && reqEnd >= endTime)
         ) {
           isSlotBusy = true;
-          // console.log(new Date(item.time), "timeToCheck", timeToCheck);
-          // console.log(new Date(item.lastTime), "lastTime");
-          // console.log(new Date(time.start), "startTime", startTime);
-          // console.log(new Date(time.end), "endTime", endTime);
           break;
         }
       }
@@ -568,16 +525,13 @@ const BookNow = () => {
       }
       return item;
     });
-    console.log(dataArray, "dataarray");
     return dataArray;
   };
 
   useEffect(() => {
     // if the date is valid after the conditions then it will be marked as isEventBusy: true
     let renderTimeArray = [];
-    console.log("Busy Slots");
     if (!!selectedDates) {
-      console.log("Generating Busy Slots");
       renderTimeArray = timeSlots.map((timeSlot) => {
         if (
           selectedDates === moment(timeSlot.time).format("MMM DD").toString()
@@ -602,8 +556,6 @@ const BookNow = () => {
       //   return { time: new Date(timeSlot.time), isEventBusy: false };
       // });
     }
-    console.log("renderTimeArray", renderTimeArray);
-    console.log("busyEventData", busyEventData);
 
     setRenderTimeArray(timeSlotFunction());
   }, [busyEventData, selectedDates, timeSlots]);
