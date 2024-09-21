37ffe29febaf8fb6e104c571d24c63843beb5c4a
SaifAlam <kaif0350@gmail.com>
2024-09-21 22:58:02 +0530

    Profile Completion section and Image Section has been completed

--- src/components/ProfileDashboard/ProfileDashboard.jsx
+++ src/components/ProfileDashboard/ProfileDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./profiledashboard.css";
 import ProfileWithPostEditShare from "./ProfileWithPostEditShare/ProfileWithPostEditShare";
+import ProfileCompletionSection from "./ProfileCompletionSection/ProfileCompletionSection";
 
 const ProfileDashboard = () => {
   return (
@@ -9,6 +10,17 @@ const ProfileDashboard = () => {
         <div className="main-profile-dashboard-panel">
           <div className="main-profile-dashboard-left">
             <ProfileWithPostEditShare />
+            <ProfileCompletionSection />
+
+            <div style={{ marginTop: 20, borderRadius: 8 }}>
+              <img
+                style={{ borderRadius: 8 }}
+                src="./rectangle-img.png"
+                width={367}
+                height={367}
+                alt=""
+              />
+            </div>
           </div>
           <div
             style={{ border: "1px solid gray" }}
