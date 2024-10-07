126936739e5cedebbf0d3e5c249d2490884579fb
Swapnil Raj <swaraj@elnovalabs.com>
2024-10-07 19:17:19 +0530

    feat: Update redirection to authentication code to consider search params

--- src/features/redirectToAuth.jsx
+++ src/features/redirectToAuth.jsx
@@ -2,7 +2,11 @@ const redirectToAuth = (path, redirectPath = "") => {
   sessionStorage.setItem("redirectToAuth", true);
   sessionStorage.setItem(
     "redirectToAuthLink",
-    !redirectPath ? window.location.pathname : redirectPath
+    !redirectPath
+      ? `${window.location.pathname}${
+          window.location.search ? window.location.search : ""
+        }`
+      : redirectPath
   );
   window.location.href = `${path}`;
 };
