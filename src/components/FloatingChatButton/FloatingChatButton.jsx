505e2c85e0344df9b606b3c61cf99aed74d02e30
SaifAlam <kaif0350@gmail.com>
2024-12-11 22:38:36 +0530

    Resolved the button issue of chat which wass showing on the services page

--- src/components/FloatingChatButton/FloatingChatButton.jsx
+++ src/components/FloatingChatButton/FloatingChatButton.jsx
@@ -10,6 +10,7 @@ export default function FloatingChatButton() {
   const { isChatOpen, setIsChatOpen, setNavigateBackTo, lastOpenChat } =
     useCommunityChat();
   const [width, setWidth] = useState(window.innerWidth);
+  const [displayChatButton, setDisplayChatButton] = useState(true);
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
@@ -41,41 +42,49 @@ export default function FloatingChatButton() {
     setIsChatOpen(false);
   }, [location.pathname]);
 
+  useEffect(() => {
+    if (location.pathname.includes("referrals")) {
+      setDisplayChatButton(false);
+    } else setDisplayChatButton(true);
+  }, [location]);
+
   const handleOpenChat = () => {
     setNavigateBackTo(location.pathname + location.search);
     setIsChatOpen(true);
   };
 
-  return location.pathname.includes("/chat/") ? null : (
-    <button
-      id="floating-chat-button"
-      onClick={handleOpenChat}
-      className="body-lg-regular"
-      style={{
-        position: "fixed",
-        zIndex: 1000,
-        transition: "all 0.3s ease-in-out",
-        opacity: !isChatOpen ? 1 : 0,
-        bottom: !isChatOpen ? (width > 820 ? "1rem" : "4.25rem") : "0",
-        right: !isChatOpen ? (width > 820 ? "1rem" : ".25rem") : "0",
-        height: !isChatOpen ? "60px" : "100vh",
-        width: !isChatOpen ? (width > 820 ? "252px" : "108px") : "100vw",
-        borderRadius: !isChatOpen ? "24px 24px 0 24px" : "0",
-        boxShadow: !isChatOpen ? "0 0 10px 0 rgba(0, 0, 0, 0.1)" : "none",
-        padding: "1rem",
-        backgroundColor: "#138382",
-        color: "white",
-        border: "none",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "16px",
-      }}
-    >
-      {width > 820 ? "Community Chat" : "Chat"}
-      <div className="chat-button-icon">
-        <IoIosArrowUp />
-      </div>
-    </button>
-  );
+  return location.pathname.includes("/chat/")
+    ? null
+    : displayChatButton && (
+        <button
+          id="floating-chat-button"
+          onClick={handleOpenChat}
+          className="body-lg-regular"
+          style={{
+            position: "fixed",
+            zIndex: 1000,
+            transition: "all 0.3s ease-in-out",
+            opacity: !isChatOpen ? 1 : 0,
+            bottom: !isChatOpen ? (width > 820 ? "1rem" : "4.25rem") : "0",
+            right: !isChatOpen ? (width > 820 ? "1rem" : ".25rem") : "0",
+            height: !isChatOpen ? "60px" : "100vh",
+            width: !isChatOpen ? (width > 820 ? "252px" : "108px") : "100vw",
+            borderRadius: !isChatOpen ? "24px 24px 0 24px" : "0",
+            boxShadow: !isChatOpen ? "0 0 10px 0 rgba(0, 0, 0, 0.1)" : "none",
+            padding: "1rem",
+            backgroundColor: "#138382",
+            color: "white",
+            border: "none",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "16px",
+          }}
+        >
+          {width > 820 ? "Community Chat" : "Chat"}
+          <div className="chat-button-icon">
+            <IoIosArrowUp />
+          </div>
+        </button>
+      );
 }
