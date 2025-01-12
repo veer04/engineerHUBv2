6c303fc38f758420133cda7ed087c96daa1fd0d0
SaifAlam <kaif0350@gmail.com>
2025-01-12 17:28:54 +0530

    Added general blog

---
+++ src/components/NewBlogCard/NewBlogCard2.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./NewBlogCard.css";
+import defaultPoster, {
+  defaultBlogPoster,
+  defaultProjectPoster,
+  eHUBLogo,
+} from "../../assets/defaultPoster";
+import { useNavigate } from "react-router-dom";
+
+export default function NewBlogCard2({ blog }) {
+  const navigate = useNavigate();
+  return (
+    <article
+      onClick={() => navigate(`/community/blogs/General/${blog._id}`)}
+      className="blog-card"
+    >
+      <div className="poster-container">
+        <img
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = defaultBlogPoster;
+          }}
+          src={blog?.postIcon}
+          alt={`${blog.title} poster`}
+        />
+        <div className="tags">
+          {blog?.techStack?.slice(0, 2).map((tag) => (
+            <span key={tag} className="tag">
+              {tag}
+            </span>
+          ))}
+          {blog?.techStack?.length > 2 && (
+            <span className="tag">+{blog?.techStack?.length - 2}</span>
+          )}
+        </div>
+      </div>
+      <p className="heading text-crop-2" title={`${blog?.title}`}>
+        {blog?.title}
+      </p>
+      {/* <p
+        className="description text-crop-1"
+        dangerouslySetInnerHTML={{
+          __html: blog?.postArea,
+        }}
+      /> */}
+      <div className="info-container">
+        <div className="logo">
+          <img
+            onError={(e) => {
+              // e.target.onerror = null;
+              e.target.src = eHUBLogo;
+            }}
+            src={eHUBLogo}
+            alt={`${blog?.creatorId?.name ? blog?.creatorId?.name : ""} logo`}
+            loading="lazy"
+          />
+        </div>
+        <div className="details">
+          <span className="title">Created By</span>
+          <span className="name text-crop-1">engineerHUB</span>
+        </div>
+      </div>
+    </article>
+  );
+}
