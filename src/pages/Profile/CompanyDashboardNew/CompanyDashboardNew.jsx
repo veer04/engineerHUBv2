46e9a952c1946a2fad82d0f13ea3e1068e392354
SaifAlam <kaif0350@gmail.com>
2025-02-18 22:40:29 +0530

    created a new third and fourth section

--- src/pages/Profile/CompanyDashboardNew/CompanyDashboardNew.jsx
+++ src/pages/Profile/CompanyDashboardNew/CompanyDashboardNew.jsx
@@ -4,6 +4,8 @@ import "./companydashboardnew.css";
 import NewCompanyDashboardHeader from "./NewCompanyDashboardHeader/NewCompanyDashboardHeader";
 import HostPageForProfileDashboard from "../../../components/ProfileDashboard/HostpageForProfileDashboard/HostPageForProfileDashboard";
 import AboutCompNewCompany from "./AboutCompNewCompany/AboutCompNewCompany";
+import NewCompanyThirdAndFourthSec from "./SectionThreeAndFourNewCompany/NewCompanyThirdAndFourthSec";
+import { Bucket_URL } from "../../../services/APIUtils";
 
 const CompanyDashboardNew = () => {
   return (
@@ -16,8 +18,34 @@ const CompanyDashboardNew = () => {
         <AboutCompNewCompany />
       </div>
 
-      <div>
+      <div style={{ marginBottom: 20 }}>
         <HostPageForProfileDashboard />
+      </div>
+
+      <div style={{ marginBottom: 20 }}>
+        <NewCompanyThirdAndFourthSec
+          title={"4x your campus placements"}
+          desc={
+            "From connecting with 5 lakh+ engineers to assessments, virtual interviews, advanced analytics, and ATS-powered shortlisting—our platform automates and streamlines every step of the recruitment journey."
+          }
+          bgColor={"#E8BA9826"}
+          btn={"For Companies"}
+          btnLink={"/about"}
+          rightImage={`${Bucket_URL}newcompanydashboard/right_frame1.png`}
+        />
+      </div>
+
+      <div>
+        <NewCompanyThirdAndFourthSec
+          title={"70% less screening time"}
+          desc={
+            "Accelerate your hiring process with our streamlined platform—connect, assess, and onboard top talent within 72 hours, ensuring efficiency and quality every step of the way."
+          }
+          bgColor={"#8FC8E826"}
+          btn={"For HRs"}
+          btnLink={"/about"}
+          rightImage={`${Bucket_URL}newcompanydashboard/right_frame2.png`}
+        />
       </div>
     </main>
   );
---
+++ src/pages/Profile/CompanyDashboardNew/SectionThreeAndFourNewCompany/NewCompanyThirdAndFourthSec.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./newcompanythirdandfourth.css";
+
+const NewCompanyThirdAndFourthSec = ({
+  title,
+  desc,
+  bgColor,
+  btn,
+  btnLink,
+  rightImage,
+}) => {
+  return <div></div>;
+};
+
+export default NewCompanyThirdAndFourthSec;
