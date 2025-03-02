7525fc80d6935d82002dfb939e48d2624f71ee3d
SaifAlam <kaif0350@gmail.com>
2025-03-02 17:29:35 +0530

    created a organization sidebar and a little bit of alumni sidebar

---
+++ src/components/ProfileSection/ProfilePopUp/NewProfileConnectCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./newprofileconnectcard.css";
+import {
+  ArrowSvgCompright,
+  Card1ImageSvgProfileSidebar,
+} from "../../SvgsIconsComps/SvgsComps";
+import { useNavigate } from "react-router-dom";
+
+const NewProfileConnectCard = ({ image, bgColor, title, btnName, btnLink }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="main-connect-card-div" style={{ background: bgColor }}>
+      <div className="main-connect-sub-div">
+        <div className="left-div-card">
+          <h4 className="connect-h4">{title}</h4>
+
+          <div className="connect-button-div">
+            <button onClick={() => navigate(btnLink)}>
+              {btnName}
+              <ArrowSvgCompright />
+            </button>
+          </div>
+        </div>
+
+        <div className="right-div-card">{image}</div>
+      </div>
+    </div>
+  );
+};
+
+export default NewProfileConnectCard;
