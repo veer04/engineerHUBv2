db7521828d5898c3a6d05dd0298fdb08646cc396
SaifAlam <kaif0350@gmail.com>
2025-05-05 18:51:55 +0530

    done with the bugs

---
+++ src/pages/Enterprise/Assests/Components/EasyWayToHire.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./easywaytohire.css";
+
+const EasyWayToHire = () => {
+  return (
+    <div className="main-easy-way-to-hire-div">
+      <div className="h3-3easy-hire-div">
+        <h3 className="h3-3easy-hire">
+          3 easy ways to{" "}
+          <span style={{ backgroundColor: "#fdf751" }}> Hire</span> through
+          engineerHUB{" "}
+        </h3>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default EasyWayToHire;
