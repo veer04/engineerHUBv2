bb58654b01a36358ff1f7d07f31f81bf5208f317
veer04 <681veerkaran@gmail.com>
2025-05-13 13:37:29 +0530

    updating pop up

--- src/pages/Enterprise/Assests/Components/EasyWayToHire/EasyWayModal.jsx
+++ src/pages/Enterprise/Assests/Components/EasyWayToHire/EasyWayModal.jsx
@@ -1,4 +1,47 @@
+
+// EasyWayModal.jsx
 import React from "react";
+import { MdEmail, MdPhone } from "react-icons/md";
+import "./easymodal.css";
+
+const EasyWayModal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const handleEmailClick = () => {
+    window.open("mailto:info@engineerhub.in", "_blank");
+  };
+
+  const handleWhatsAppClick = () => {
+    const phoneNumber = "+918303156089";
+    window.open(`https://wa.me/${phoneNumber}`, "_blank");
+  };
+
+  return (
+    <div className="modal-overlay-easyway">
+      <div className="modal-box-easyway">
+        <h3 className="modal-title">Kindly drop a call or mail</h3>
+
+        <div className="modal-contact" onClick={handleEmailClick}>
+          <MdEmail className="modal-icon" />
+          <span>info@engineerhub.in</span>
+        </div>
+
+        <div className="modal-contact" onClick={handleWhatsAppClick}>
+          <MdPhone className="modal-icon" />
+          <span>+91 8303156089</span>
+        </div>
+
+        <button className="modal-back-btn" onClick={onClose}>
+          Back
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EasyWayModal;
+
+{/*import React from "react";
 import "./easymodal.css";
 
 const EasyWayModal = ({ isOpen, onClose }) => {
@@ -40,3 +83,4 @@ const EasyWayModal = ({ isOpen, onClose }) => {
 };
 
 export default EasyWayModal;
+*/}
