58b49a76c93b7c48cb472e3e7d7c3ad29d068e36
veer04 <681veerkaran@gmail.com>
2025-08-12 21:05:51 +0530

    testimonial changes done

--- server_files/script.sh
+++ server_files/script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-serve -s /app/dist >> /logs/server.log &
+# Start serve in the background
+serve -s /app/dist -l 3000 >> /logs/server.log 2>&1 &
+
+# Start nginx in the foreground (this keeps the container running)
 nginx -g "daemon off;" 
